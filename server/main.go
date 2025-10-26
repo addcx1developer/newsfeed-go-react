57e38033aef3a24d92d8bbc2e904c9eb8d9fb7c9
ALEXIS D. DELA CRUZ <addcx1developer@gmail.com>
2025-10-26 13:23:28 +0800

    Add go-chi package

--- server/main.go
+++ server/main.go
@@ -1,3 +1,18 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func main() {
+	r := chi.NewRouter()
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Hello, World!"))
+	})
+
+	http.ListenAndServe(":8080", r)
+}

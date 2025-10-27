a5a0fe96b99cf54d86e338c9279431b54213f5b2
ALEXIS D. DELA CRUZ <addcx1developer@gmail.com>
2025-10-27 15:14:59 +0800

    Add auto stage files

--- scripts/printSDL.mjs
+++ scripts/printSDL.mjs
@@ -1,7 +1,7 @@
 import { buildClientSchema, printSchema } from "graphql";
 import fs from "fs";
 
-const [,, inputFile, outputFile] = process.argv;
+const [, , inputFile, outputFile] = process.argv;
 const introspection = JSON.parse(fs.readFileSync(inputFile, "utf-8"));
 
 const schema = buildClientSchema(introspection);
